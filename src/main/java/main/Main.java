9b4ee86659612093c71ce7cbb0b16f5c57b9b4b6
Kornel Sokolowski <matters@poczta.onet.pl>
2019-02-22 19:35:59 +0100

    moje zmiany

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,23 +1,45 @@
 package main;
 
-import model.Auction;
-import model.AuctionService;
-import model.Customer;
+import model.*;
 
 import java.time.LocalDateTime;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         Customer seller = new Customer("Jan Kowalski");
-        Auction auction = new Auction(1,"Coffee",null,
-                LocalDateTime.now().plusDays(2L),seller,0,null);
+        Auction auction = new Auction.AuctionBuilder()
+                .withId(1)
+                .withItem("Coffee")
+                .withDescription(null)
+                .withLocalDateTime(LocalDateTime.now().plusDays(2L))
+                .withSeller(seller)
+                .withHighestBid(0)
+                .withBuyer(null)
+                .build();
 
-        AuctionService auctionService = new AuctionService();
-        auctionService.bid(auction,new Customer("Anna Kowalik"),20);
+        // new Auction(1,"Coffee",null,
+        // LocalDateTime.now().plusDays(2L),seller,0,null);
+        AuctionNotifier auctionNotifier = new AuctionNotifierImp();
+        Customer customerOne = new Customer("Anna Kowalik");
+        Customer customerTwo = new Customer("Krzysztof Adamowicz");
+        Customer customerThree = new Customer("Krzysztof Kolumb");
+        auctionNotifier.registerObserver(customerOne);
+        auctionNotifier.registerObserver(customerTwo);
+        auctionNotifier.registerObserver(customerThree);
 
-        auctionService.bid(auction,new Customer("Krzysztof Adamowicz"),25);
+        AuctionService auctionService = AuctionService.getInstance();
+        auctionService.bid(auction, customerOne, 20);
+        auctionService.bid(auction, customerTwo, 25);
+        auctionService.bid(auction, customerThree, 30);
 
+        auctionNotifier.notifyObservers();
 
-        System.out.println("Auction with id: " + auction.getId()+ " finished. Highest bid: " + auction.getHighestBid() + " PLN.");
+        auctionService.bid(auction, customerTwo, 35);
+        auctionNotifier.notifyObservers();
+        customerOne.printNotifications();
+        customerThree.printNotifications();
+        customerTwo.printNotifications();
+
+        System.out.println("Auction with id: " + auction.getId() + " finished. Highest bid: " + auction.getHighestBid() + " PLN.");
     }
 }
--- src/main/java/model/Customer.java
+++ src/main/java/model/Customer.java
@@ -1,9 +1,22 @@
 package model;
 
-public class Customer {
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Customer implements Observer {
     private String fullName;
+    protected List<String> notifications = new ArrayList<>();
 
     public Customer(String fullName) {
         this.fullName = fullName;
+    }
+
+    @Override
+    public void update() {
+        notifications.add(LocalDateTime.now() + " " + "You have been overbided");
+    }
+    public void printNotifications() {
+        System.out.println(notifications);
     }
 }
